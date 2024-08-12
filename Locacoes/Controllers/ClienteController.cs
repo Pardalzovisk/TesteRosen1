587b07553ad497541c20cf8de1aa5506924deb93
rosenclever.gazoni <rosenclever.gazoni@TPCP04LAB1000>
2024-08-12 20:29:20 -0300

    Adicionar arquivos de projeto.

---
+++ Locacoes/Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+﻿using Locacoes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Locacoes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public static List<Cliente> Clientes = new List<Cliente>()
+        {
+            new Cliente()
+            {
+                Id = 1,
+                Nome = "Rebeca",
+                Email = "rebecaAndrade@mail.com",
+                Telefone = "(21)999887799",
+                Cidade = "Rio de Janeiro"
+            },
+            new Cliente()
+            {
+                Id = 2,
+                Nome = "Medina",
+                Email = "gabrielMedina@mail.com",
+                Telefone = "(14)998887744",
+                Cidade = "Santos"
+            }
+
+        };
+        public IActionResult Index()
+        {
+            return View(Clientes);
+        }
+    }
+}
