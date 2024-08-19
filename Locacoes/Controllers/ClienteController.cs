d88d6387224c829eb231d5527e3bd34a1682cd4a
rosenclever <rosenclever@gmail.com>
2024-08-19 20:56:09 -0300

    crud completo cliente (falta corrigir problema de inclusão com lista vazia)

--- Locacoes/Controllers/ClienteController.cs
+++ Locacoes/Controllers/ClienteController.cs
@@ -44,5 +44,37 @@ namespace Locacoes.Controllers
             Clientes.Add(cliente);
             return RedirectToAction("Index");
         }
+        public IActionResult Edit(int id)
+        {
+            return View(Clientes.Where(cli => cli.Id == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Cliente cliente)
+        {
+            var clienteOld = Clientes.Where(cli => cli.Id == cliente.Id).First();
+            Clientes.Remove(clienteOld);
+            Clientes.Add(cliente); 
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(Clientes.Where(cli => cli.Id == id).First());
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View(Clientes.Where(cli => cli.Id == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var cliente = Clientes.Where(cli => cli.Id == id).First();
+            Clientes.Remove(cliente);
+            return RedirectToAction("Index");
+        }
+
     }
 }
