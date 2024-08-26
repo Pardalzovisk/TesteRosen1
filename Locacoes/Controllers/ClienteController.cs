e59836800188c679d204e5326e88c85e2f901439
rosenclever <rosenclever@gmail.com>
2024-08-26 20:51:06 -0300

    Entity Configurado e action Index funcionando

--- Locacoes/Controllers/ClienteController.cs
+++ Locacoes/Controllers/ClienteController.cs
@@ -1,34 +1,24 @@
-﻿using Locacoes.Models;
+﻿using Locacoes.Data;
+using Locacoes.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace Locacoes.Controllers
 {
     public class ClienteController : Controller
     {
-        public static List<Cliente> Clientes = new List<Cliente>()
-        {
-            new Cliente()
-            {
-                Id = 1,
-                Nome = "Rebeca",
-                Email = "rebecaAndrade@mail.com",
-                Telefone = "(21)999887799",
-                Cidade = "Rio de Janeiro"
-            },
-            new Cliente()
-            {
-                Id = 2,
-                Nome = "Medina",
-                Email = "gabrielMedina@mail.com",
-                Telefone = "(14)998887744",
-                Cidade = "Santos"
-            }
+        private readonly LocacoesContext _context;
 
-        };
-        public IActionResult Index()
+        public ClienteController(LocacoesContext context)
         {
-            return View(Clientes);
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clientes.OrderBy(cli => cli.Nome).ToListAsync());
         }
 
         public IActionResult Create()
@@ -36,45 +26,45 @@ namespace Locacoes.Controllers
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Cliente cliente)
-        {
-            cliente.Id = Clientes.Select(x => x.Id).Max() + 1; 
-            Clientes.Add(cliente);
-            return RedirectToAction("Index");
-        }
-        public IActionResult Edit(int id)
-        {
-            return View(Clientes.Where(cli => cli.Id == id).First());
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Cliente cliente)
-        {
-            var clienteOld = Clientes.Where(cli => cli.Id == cliente.Id).First();
-            Clientes.Remove(clienteOld);
-            Clientes.Add(cliente); 
-            return RedirectToAction("Index");
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Create(Cliente cliente)
+        //{
+        //    cliente.Id = Clientes.Select(x => x.Id).Max() + 1; 
+        //    Clientes.Add(cliente);
+        //    return RedirectToAction("Index");
+        //}
+        //public IActionResult Edit(int id)
+        //{
+        //    return View(Clientes.Where(cli => cli.Id == id).First());
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Edit(Cliente cliente)
+        //{
+        //    var clienteOld = Clientes.Where(cli => cli.Id == cliente.Id).First();
+        //    Clientes.Remove(clienteOld);
+        //    Clientes.Add(cliente); 
+        //    return RedirectToAction("Index");
+        //}
 
-        public IActionResult Details(int id)
-        {
-            return View(Clientes.Where(cli => cli.Id == id).First());
-        }
+        //public IActionResult Details(int id)
+        //{
+        //    return View(Clientes.Where(cli => cli.Id == id).First());
+        //}
 
-        public IActionResult Delete(int id)
-        {
-            return View(Clientes.Where(cli => cli.Id == id).First());
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var cliente = Clientes.Where(cli => cli.Id == id).First();
-            Clientes.Remove(cliente);
-            return RedirectToAction("Index");
-        }
+        //public IActionResult Delete(int id)
+        //{
+        //    return View(Clientes.Where(cli => cli.Id == id).First());
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult DeleteConfirmed(int id)
+        //{
+        //    var cliente = Clientes.Where(cli => cli.Id == id).First();
+        //    Clientes.Remove(cliente);
+        //    return RedirectToAction("Index");
+        //}
 
     }
 }
--- Locacoes/Program.cs
+++ Locacoes/Program.cs
@@ -1,7 +1,13 @@
+using Locacoes.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("LocAutoConnection");
+builder.Services.AddDbContext<LocacoesContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
